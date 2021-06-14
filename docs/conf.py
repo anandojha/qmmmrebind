2db75e58704f01b9c8cf7ac0c608ca3ba6af6c01
anandojha <anandojha.002@gmail.com>
2021-06-14 15:01:08 -0700

    remove docsV

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
-#import qmmmrebind
+import qmmmrebind
 
 # -- Project information -----------------------------------------------------
 
