37857da20467734b212b077cb6a90f65967c7703
anandojha <anandojha.002@gmail.com>
2021-08-29 12:36:26 -0400

    pinned openmm version to 7.5.0 (incompatible 7.6 version with parmed)

--- qmmmrebind/tests/test_qmmmrebind.py
+++ qmmmrebind/tests/test_qmmmrebind.py
@@ -217,14 +217,6 @@ def test_xyz_to_pdb():
             atom_list.append(line)
     assert len(atom_list) == 18
 
-def test_copy_system_init_xml():
-    source_ = get_data_filename("test_guest_init.xml")
-    destination_pwd = os.getcwd()
-    destination_file = source_.split("/")[-1]
-    destination_ = destination_pwd + "/" + destination_file
-    qmmmrebind.parameterize.copy_file(source=source_, destination=destination_)
-    assert "test_guest_init.xml" in os.listdir()
-
 def test_generate_xml_from_charged_pdb_sdf():
     system_pdb = "test_torsion_drive_input.pdb"
     system_init_sdf = "test_guest_init.sdf"
